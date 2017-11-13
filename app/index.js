e870c6661c431811e2447c33eb6a6475131e12b0
Dawid Zegar <davvvid1@gmail.com>
2017-11-13 13:24:13 +0100

    Change end line sign to windows format

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(bodyParser.text());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Poker hands digger server is running!'));
 
 function printRound(name, data, players, previousCards, bet = 0)
 {
@@ -16,24 +16,24 @@ function printRound(name, data, players, previousCards, bet = 0)
         }
         output += `[${data.CARD}]`;
     }
-    output += ` ***\n`;
+    output += ` ***\r\n`;
 
     data.PLAYER.forEach((object) =>
     {
         output += `${players[object.NUMBER - 1].name}: `;
         if ('fold 0' === object.ACTION) {
-            output += 'folds\n';
+            output += 'folds\r\n';
         } else if ('check 0' === object.ACTION) {
-            output += 'checks\n';
+            output += 'checks\r\n';
         } else if (_.includes(object.ACTION, 'call')) {
-            output += `calls $${object.ACTION.slice(5)}\n`;
+            output += `calls $${object.ACTION.slice(5)}\r\n`;
         } else if (_.includes(object.ACTION, 'raise')) {
             const coins = Number(object.ACTION.slice(6));
             if (bet) {
-                output += `raises $${coins} to $${coins + bet}\n`;
+                output += `raises $${coins} to $${coins + bet}\r\n`;
             } else {
                 bet = coins;
-                output += `bets $${coins}\n`;
+                output += `bets $${coins}\r\n`;
             }
         }
     });
@@ -64,14 +64,14 @@ app.post('/parse', (req, res) =>
     let winPlayers = [];
     let previousCards = '';
     let output = '';
-    output += `PokerStars Hand #${Date.now()}:  Hold'em No Limit ($${smallBlind}/$${bigBlind} USD) - ${parsedBody.TIME}\n`;
-    output += `Table '${parsedBody.TIME}' ${table.SEATS}-max Seat #${table.DEALER} is the button\n`;
+    output += `PokerStars Hand #${Date.now()}:  Hold'em No Limit ($${smallBlind}/$${bigBlind} USD) - ${parsedBody.TIME}\r\n`;
+    output += `Table '${parsedBody.TIME}' ${table.SEATS}-max Seat #${table.DEALER} is the button\r\n`;
     players.forEach((player) =>
     {
-        output += `Seat ${player.number}: ${player.name} ($${player.chips} in chips)\n`;
+        output += `Seat ${player.number}: ${player.name} ($${player.chips} in chips)\r\n`;
     });
-    output += `${players[0].name}: posts small blind $${smallBlind}\n`;
-    output += `${players[1].name}: posts big blind $${bigBlind}\n`;
+    output += `${players[0].name}: posts small blind $${smallBlind}\r\n`;
+    output += `${players[1].name}: posts big blind $${bigBlind}\r\n`;
     output += printRound('HOLE CARDS', game.PREFLOP, players, null, bigBlind);
     if (game.FLOP) {
         output += printRound('FlOP', game.FLOP, players);
@@ -85,7 +85,7 @@ app.post('/parse', (req, res) =>
         output += printRound('RIVER', game.RIVER, players, previousCards);
     }
     if (1 < parsedBody.SHOWDOWN.PLAYER.length) {
-        output += `*** SHOW DOWN ***\n`;
+        output += `*** SHOW DOWN ***\r\n`;
         parsedBody.SHOWDOWN.PLAYER.forEach((player) =>
         {
             if (_.includes(player.ACTION, 'win')) {
@@ -93,9 +93,9 @@ app.post('/parse', (req, res) =>
                 players[player.NUMBER - 1].win = player.ACTION.slice(4);
             }
             if (_.includes(player.ACTION, 'noshow')) {
-                output += `${players[player.NUMBER - 1].name}: mucks hand\n`;
+                output += `${players[player.NUMBER - 1].name}: mucks hand\r\n`;
             } else {
-                output += `${players[player.NUMBER - 1].name}: shows [${player.CARD}]\n`;
+                output += `${players[player.NUMBER - 1].name}: shows [${player.CARD}]\r\n`;
             }
         })
     } else {
@@ -109,16 +109,16 @@ app.post('/parse', (req, res) =>
     }
     winPlayers.forEach((player) =>
     {
-        output += `${players[player.NUMBER - 1].name} collected ${players[player.NUMBER - 1].win} from pot\n`;
+        output += `${players[player.NUMBER - 1].name} collected ${players[player.NUMBER - 1].win} from pot\r\n`;
     });
-    output += `*** SUMMARY ***\n`;
+    output += `*** SUMMARY ***\r\n`;
     players.forEach((player) =>
     {
         if (player.win) {
-            output += `Seat ${player.number}: ${player.name} collected $${player.win}\n`;
+            output += `Seat ${player.number}: ${player.name} collected $${player.win}\r\n`;
 
         } else {
-            output += `Seat ${player.number}: ${player.name} folded\n`;
+            output += `Seat ${player.number}: ${player.name} folded\r\n`;
         }
     });
 
@@ -127,4 +127,4 @@ app.post('/parse', (req, res) =>
     res.send(output);
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log(`Poker hands digger server listening on port ${process.env.PORT || 3000}`));
